06037d5ad0f1ef551cba4290f68224781785f0e2
aticicaner <aticicaner@gmail.com>
2019-07-12 12:17:13 -0700

    first commit

---
+++ hw3.py
@@ -0,0 +1,16 @@
+def collatz(num):
+    if (num % 2 == 0):
+        return num / 2
+    elif (num % 2 == 1):
+        return num * 3 + 1
+    
+print("enter a positive integer: ")
+
+try:
+    num_input = int(input())
+    while(num_input > 1):
+        num_input = int(collatz(num_input))
+        print(num_input)
+except:
+    print("Please use numeric values for an integer")
+
